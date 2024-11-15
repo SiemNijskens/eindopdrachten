511524d8af52809bf2d6fd796aec44f80ae326bf
siemnijskens <siemnijskens@gmail.com>
2024-11-15 15:56:36 +0100

    eindopdracht week 2 ook af

--- src/EindopdrachWeek1.java
+++ src/EindopdrachWeek1.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class EindopdrachWeek1 {
     public static void main(String[] args) {
         char[] woordarray = getInput("woord");
 
--- src/EindopdrachtWeek2.java
+++ src/EindopdrachtWeek2.java
@@ -12,23 +12,13 @@ public class EindopdrachtWeek2 {
         int somEvenGetallen = 0;
         int somOnevenGetallen = 0;
 
-        if (getal % 2 == 0) {
-            for (int i = 0; i <= (getal); i = i + 2) {
-                somEvenGetallen += i;
-            }
-            for (int j = 1; j <= (getal); j = j + 2) {
-                somOnevenGetallen += j;
-            }
-
-        } else {
-            for (int i = 0; i <= (getal); i = i + 2) {
-                somEvenGetallen += i;
-            }
-            for (int j = 1; j <= (getal); j = j + 2) {
-                somOnevenGetallen += j;
-            }
-
+        for (int i = 0; i <= (getal); i = i + 2) {
+            somEvenGetallen += i;
         }
+        for (int j = 1; j <= (getal); j = j + 2) {
+            somOnevenGetallen += j;
+        }
+
         System.out.println("De som van de even getallen tot en met " + getal + " is " + somEvenGetallen);
         System.out.println("De som van de oneven getallen tot en met " + getal + " is " + somOnevenGetallen);
 
