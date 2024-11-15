6829a7fa5181c784c01a56b74145be1cbd51318c
siemnijskens <siemnijskens@gmail.com>
2024-11-15 13:41:39 +0100

    eindopdracht week 2 ook af

---
+++ src/EindopdrachtWeek2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EindopdrachtWeek2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int getal;
+        do {
+            System.out.println("Geef een geheel positief getal");
+            getal = scanner.nextInt();
+        } while (getal < 1);
+
+        int somEvenGetallen = 0;
+        int somOnevenGetallen = 0;
+
+        if (getal % 2 == 0) {
+            for (int i = 0; i <= (getal); i = i + 2) {
+                somEvenGetallen += i;
+            }
+            for (int j = 1; j <= (getal); j = j + 2) {
+                somOnevenGetallen += j;
+            }
+
+        } else {
+            for (int i = 0; i <= (getal); i = i + 2) {
+                somEvenGetallen += i;
+            }
+            for (int j = 1; j <= (getal); j = j + 2) {
+                somOnevenGetallen += j;
+            }
+
+        }
+        System.out.println("De som van de even getallen tot en met " + getal + " is " + somEvenGetallen);
+        System.out.println("De som van de oneven getallen tot en met " + getal + " is " + somOnevenGetallen);
+
+        System.out.println("het verschil tussen de twee sommen is " + (somEvenGetallen - somOnevenGetallen));
+    }
+}
